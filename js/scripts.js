fa351eb083879627b875c966816750f093561e9b
Daniel Julian <dani_77@live.com.ar>
2024-09-08 00:28:39 -0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,386 @@
+const chicos = [
+    {
+        "name": "Roty",
+        "region": "ciudad evita",
+        "trabaja": "no",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "roba novias"
+    },
+    {
+        "name": "Ima",
+        "region": "avellaneda",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanquisimo",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "no se sabe si es pelado"
+    },
+    {
+        "name": "Kener",
+        "region": "rosario",
+        "trabaja": "no",
+        "pelo": "si",
+        "color_de_piel": "blanquisimo",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "el nene en la nena"
+    },
+    {
+        "name": "Juanma",
+        "region": "cordoba",
+        "trabaja": "si",
+        "pelo": "no",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "bruiser"
+    },
+    {
+        "name": "Eri",
+        "region": "caseros",
+        "trabaja": "si",
+        "pelo": "no",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "milf"
+    },
+    {
+        "name": "Crilzo",
+        "region": "porteño",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "bizco"
+    },
+    {
+        "name": "Calippy",
+        "region": "el void",
+        "trabaja": "no",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "crazy taco"
+    },
+    {
+        "name": "Fede",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "bueno",
+        "es_gil": "no",
+        "lore": "buena ceja"
+    },
+    {
+        "name": "Nabu",
+        "region": "san martin",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "average",
+        "es_gil": "no",
+        "lore": "ruido de encendedor"
+    },
+    {
+        "name": "Dano",
+        "region": "avellaneda",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "bueno",
+        "es_gil": "no",
+        "lore": "maestro splinter"
+    },
+    {
+        "name": "Sighma",
+        "region": "jose c paz",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "ilegal es poco"
+    },
+    {
+        "name": "Agu",
+        "region": "mar del plata",
+        "trabaja": "no",
+        "pelo": "no",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "es buen loco"
+    },
+    {
+        "name": "Pani",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "marronazo",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "se pinta las uñas y le entra a la prima"
+    },
+    {
+        "name": "Dolla",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "chicito"
+    },
+    {
+        "name": "Chiru",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "tremendo autista"
+    },
+    {
+        "name": "Tito",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "casi muere"
+    },
+    {
+        "name": "Dyrude",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "morocho",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "esta mimosho"
+    },
+    {
+        "name": "Piox",
+        "region": "wilde",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "le re cuesta"
+    },
+    {
+        "name": "Ficha",
+        "region": "dominico",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "acido"
+    },
+    {
+        "name": "Kevin recke",
+        "region": "caseros",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "fajaba a la jermu"
+    },
+    {
+        "name": "Gmauro",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "average",
+        "es_gil": "no",
+        "lore": "gmauro"
+    },
+    {
+        "name": "Mosta",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "morocho",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "tartamudea cuando se pone nervioso"
+    },
+    {
+        "name": "Nicuco",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "el terror de chicos y grandes"
+    },
+    {
+        "name": "Wailler",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "no se calla nunca"
+    },
+    {
+        "name": "kIRAX",
+        "region": "wilde",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "es facho"
+    },
+    {
+        "name": "Fran",
+        "region": "olavarria",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "dejo los souls para dedicarse a su falcon verde"
+    }
+];
+var dataTable;
+
+const elegido = chicos[Math.floor(Math.random() * chicos.length)];
+
+function initializeAutocomplete() {
+    const chicosArray = chicos.map(chico => {
+        return {
+            label: chico.name,
+            value: chico
+        };
+    });
+
+    $('#autocomplete').autocomplete({
+        source: chicosArray,
+        lookupFilter: function (suggestion, originalQuery, queryLowerCase) {
+            var re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
+            return re.test(suggestion.value);
+        },
+        select: function (event, ui) {
+            event.preventDefault();
+
+            // Make the search bar empty
+            $('#autocomplete').val("")
+
+            // Add the row to the table
+            addRow(ui.item.value)
+        },
+    });
+}
+
+function initializeGuessTable() {
+    return new DataTable('#guesstable', {
+        info: false,
+        ordering: false,
+        paging: false,
+        searching: false,
+        rowCallback: function(row, data, index) {
+          rowCallback(row, data, index);
+        }
+    });
+}
+
+
+function verify(expected, actual, row, idx) {
+    if (expected == actual) {
+        $(row).find('td:eq(' + idx + ')').css('background-color', 'green');
+    } else {
+        $(row).find('td:eq(' + idx + ')').css('background-color', 'red');
+    }
+}
+
+function rowCallback(row, data, index) {
+    let nombre = data[0];
+    let region = data[1];
+    let trabaja = data[2];
+    let pelo = data[3];
+    let colorPiel = data[4];
+    let lol = data[5];
+    let cs = data[6];
+    let gil = data[7];
+
+    verify(nombre, elegido['name'], row, 0);
+    verify(region, elegido['region'], row, 1);
+    verify(trabaja, elegido['trabaja'], row, 2);
+    verify(pelo, elegido['pelo'], row, 3);
+    verify(colorPiel, elegido['color_de_piel'], row, 4);
+    verify(lol, elegido['habilidad_lol'], row, 5);
+    verify(cs, elegido['habilidad_cs'], row, 6);
+    verify(gil, elegido['es_gil'], row, 7);
+}
+
+function addRow(chico) {
+    $("#guesstable").show();
+
+    let row =datatable
+        .row
+        .add([
+            chico['name'],
+            chico['region'],
+            chico['trabaja'],
+            chico['pelo'],
+            chico['color_de_piel'],
+            chico['habilidad_lol'],
+            chico['habilidad_cs'],
+            chico['es_gil']
+        ])
+        .draw(false);
+    console.log(row)
+    $(row).find('td:nth-child(4)').addClass('color:green'); // PlayerA color
+}
+
+$(document).ready(function () {
+
+    initializeAutocomplete();
+
+    datatable = initializeGuessTable();
+
+})
