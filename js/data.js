5e013caebcccf50eb6278231ae1032980782b715
Daniel Julian <dani_77@live.com.ar>
2024-09-11 09:59:46 -0300

    Moved data to separate file

---
+++ js/data.js
@@ -0,0 +1,483 @@
+const chicos = [
+    {
+        "name": "Roty",
+        "region": "ciudad evita",
+        "trabaja": "no",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "roba novias"
+    },
+    {
+        "name": "Ima",
+        "region": "avellaneda",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanquisimo",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "no se sabe si es pelado"
+    },
+    {
+        "name": "Kener",
+        "region": "rosario",
+        "trabaja": "no",
+        "pelo": "si",
+        "color_de_piel": "blanquisimo",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "el nene en la nena"
+    },
+    {
+        "name": "Juanma",
+        "region": "cordoba",
+        "trabaja": "si",
+        "pelo": "no",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "bruiser"
+    },
+    {
+        "name": "Eri",
+        "region": "caseros",
+        "trabaja": "si",
+        "pelo": "no",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "milf"
+    },
+    {
+        "name": "Crilzo",
+        "region": "porteño",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "bizco"
+    },
+    {
+        "name": "Calippy",
+        "region": "el void",
+        "trabaja": "no",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "crazy taco"
+    },
+    {
+        "name": "Fede",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "bueno",
+        "es_gil": "no",
+        "lore": "buena ceja"
+    },
+    {
+        "name": "Nabu",
+        "region": "san martin",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "average",
+        "es_gil": "no",
+        "lore": "ruido de encendedor"
+    },
+    {
+        "name": "Dano",
+        "region": "avellaneda",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "bueno",
+        "es_gil": "no",
+        "lore": "maestro splinter"
+    },
+    {
+        "name": "Sighma",
+        "region": "jose c paz",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "ilegal es poco"
+    },
+    {
+        "name": "Agu",
+        "region": "mar del plata",
+        "trabaja": "no",
+        "pelo": "no",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "es buen loco"
+    },
+    {
+        "name": "Pani",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "marronazo",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "se pinta las uñas y le entra a la prima"
+    },
+    {
+        "name": "Dolla",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "chicito"
+    },
+    {
+        "name": "Chiru",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "tremendo autista"
+    },
+    {
+        "name": "Tito",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "casi muere"
+    },
+    {
+        "name": "Dyrude",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "morocho",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "esta mimosho"
+    },
+    {
+        "name": "Pioox",
+        "region": "wilde",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "le re cuesta"
+    },
+    {
+        "name": "Ficha",
+        "region": "dominico",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "bueno",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "acido"
+    },
+    {
+        "name": "Kevin recke",
+        "region": "caseros",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "average",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "fajaba a la jermu"
+    },
+    {
+        "name": "Gmauro",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "average",
+        "es_gil": "no",
+        "lore": "gmauro"
+    },
+    {
+        "name": "Mosta",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "morocho",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "tartamudea cuando se pone nervioso"
+    },
+    {
+        "name": "Nicuco",
+        "region": "mar del plata",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "el terror de chicos y grandes"
+    },
+    {
+        "name": "Wailler",
+        "region": "ciudad evita",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "si",
+        "lore": "no se calla nunca"
+    },
+    {
+        "name": "kIRAX",
+        "region": "wilde",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "bueno",
+        "es_gil": "si",
+        "lore": "es facho"
+    },
+    {
+        "name": "Fran",
+        "region": "olavarria",
+        "trabaja": "si",
+        "pelo": "si",
+        "color_de_piel": "blanco",
+        "habilidad_lol": "malo",
+        "habilidad_cs": "malo",
+        "es_gil": "no",
+        "lore": "dejo los souls para dedicarse a su falcon verde"
+    }
+];
+
+const frases_chicos = [
+{
+   "name":"Roty",
+   "frases":[
+      "Te puse",
+      "Tas jugando de reojo maxi, eh, la concha de tu madre"
+   ]
+},
+{
+   "name":"Ima",
+   "frases":[
+      "Segui ehg"
+   ]
+},
+{
+   "name":"Kener",
+   "frases":[
+      "Hijo de pootaah",
+      "Yo no me garcho a nadie.. mi jermu me mira",
+      "Puerta abierta",
+      "El tio carlo",
+      "Chogur con copos"
+   ]
+},
+{
+   "name":"Juanma",
+   "frases":[
+      "Vo so loco o te hace",
+      "Eri pli",
+      "Eee medio ct b cache",
+      "Tio carlo???"
+   ]
+},
+{
+   "name":"Eri",
+   "frases":[
+      "Y la novia para cuando?",
+      "Vo le cree"
+   ]
+},
+{
+   "name":"Crilzo",
+   "frases":[
+      "Nino vimo chico"
+   ]
+},
+{
+   "name":"Calippy",
+   "frases":[
+      "OugHhHGHHhHhgg"
+   ]
+},
+{
+   "name":"Fede",
+   "frases":[
+      "Palermitano de mierda"
+   ]
+},
+{
+   "name":"Nabu",
+   "frases":[
+      "Es todo porro, no tiene filtro"
+   ]
+},
+{
+   "name":"Dano",
+   "frases":[
+      "Si apuntas mejor, si tenes mas armas, si te moves mas y si matas mas, tenes mas chances de ganar",
+      "Pichon",
+      "Depue te plico"
+   ]
+},
+{
+   "name":"Pani",
+   "frases":[
+      "Ciego te vas a quedar de tanta droga, enfermo",
+      "Y el mono? el mono esta en tu culo"
+   ]
+},
+{
+   "name":"Dolla",
+   "frases":[
+      "Con vos? no gracia",
+      "Timagina",
+      "Yo nunca te vi"
+   ]
+},
+{
+   "name":"Pioox",
+   "frases":[
+      "Hola amor? donde estas? manda foto"
+   ]
+},
+{
+   "name":"Ficha",
+   "frases":[
+      "Estas re acido.",
+      "Si no es con empatia, no es humor."
+   ]
+},
+{
+   "name":"Mosta",
+   "frases":[
+      "Hijaaaaaa de putaaaa *se larga a llorar*"
+   ]
+},
+{
+   "name":"Nicuco",
+   "frases":[
+      "Ahora....No te puedo.....Explicar..."
+   ]
+},
+{
+   "name":"Tito",
+   "frases":[
+      "Jefe, hoy no voy",
+      "Que tan enfermo tenes que estar para violar a un hombre?"
+   ]
+},
+{
+   "name":"Wailler",
+   "frases":[
+      "Fue fue fue",
+      "Ehh?ahhh",
+      "Te calentaste"
+   ]
+},
+{
+   "name":"Mono",
+   "frases":[
+      "Como se llama la partida?"
+   ]
+},
+{
+   "name":"Fatima, hermana de nabu",
+   "frases":[
+      "Yo tambien uso el ak para matar"
+   ]
+}]
+
+const emojis_chicos = [
+
+    {
+        "name" : "Ima",
+        "icons": ["🫦","🧑‍🦲","🤑"]
+    },{
+        "name" : "Dano",
+        "icons": ["🐀","💸"]
+    },{
+        "name" : "Sighma",
+        "icons": ["🏦","🏋️","📹"]
+    },{
+        "name" : "Pani",
+        "icons": ["🤓","💅🏻","☝🏾","👨🏾‍🏫 ","😮‍💨"]
+    },{
+        "name" : "Agu",
+        "icons": ["🏋️","💅🏻","👨‍🦲 ","🇩🇪"]
+    },{
+        "name" : "Nabu",
+        "icons": ["❄️","🌱","👃"]
+    },{
+        "name" : "Fede",
+        "icons": ["📢","🦮","🤨"]
+    },{
+        "name" : "Roty",
+        "icons": ["🍜","😷","😮‍💨"]
+    },{
+        "name" : "Wailer",
+        "icons": ["🏋️","🏍️"]
+    },{
+        "name" : "Nico",
+        "icons": ["😡","☠️"]
+    },{
+        "name" : "Pioox",
+        "icons": ["😡","🏹"]
+    },{
+        "name" : "Mosta",
+        "icons": ["🛠️","💔 ","🔩","🇺🇦"]
+    },{
+        "name" : "Kener",
+        "icons": ["🥷","👱🏻"]
+    },{
+        "name" : "Mati",
+        "icons": ["🦷","🥰","🥭","🧸 "]
+    },{
+        "name" : "Tito",
+        "icons": ["🍫"]
+    }
+]
+
+
+export {
+  chicos, frases_chicos, emojis_chicos
+}
